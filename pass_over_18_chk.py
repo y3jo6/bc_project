f44bddc5e77cd79ffa86067684c9bd1d2c32a3da
y3jo6 <evolutionlin@gmail.com>
2017-01-20 01:16:42 +0800

    initial the file

---
+++ pass_over_18_chk.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import requests
+
+
+# In[6]:
+
+payload = {
+'from' : '/bbs/Gossiping/index.html',
+'yes' : 'yes'
+}
+rs = requests.session()
+res = rs.post('https://www.ptt.cc/ask/over18', verify=False, data=payload)
+res = rs.get('https://www.ptt.cc/bbs/Gossiping/index.html', verify=False)
+#res = requests.get('https://www.ptt.cc/bbs/Gossiping/index.html', verify=False)
+print (res.text)
+
+
+# In[ ]:
+
+
+
